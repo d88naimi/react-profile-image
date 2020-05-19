15b14728e40ea7afec4fac04913dbebae6fb4a24
David Naimi <d88naimi@gmail.com>
2020-05-19 13:53:38 -0700

    intial commit

---
+++ client/src/components/ProfileImage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Route, Redirect } from "react-router-dom";
+
+
+function ProfileImage() {
+    return (
+        <div>
+            <h1>Profile Image Coming Soon</h1>
+        </div>
+    )
+}
+
+export default ProfileImage;
